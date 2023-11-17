3701c78b4307734943b2443689707b873baff07e
sihaotan4 <34644190+sihaotan4@users.noreply.github.com>
2023-11-17 14:08:15 +0800

    implement Matching struct to hold matches

--- notes.md
+++
@@ -1,4 +0,0 @@
-gale-shapley always halts
-it must halt on a stable matching
-
-consider implementing preference as a BTreeSet for fast lookup of rank by checking key
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,45 @@
 use std::collections::HashMap;
 
 struct Matching {
-    //matches: Vec<(&'static str, &'static str)>
-    matches: HashMap<&'static str, &'static str>,
+    // acting as a bimap, where each match is entered twice
+    // first as (x,y) and then as (y,x) to facilitate two-way existence checks
+    matches: HashMap<String, String>,
 }
 
-#[derive(Debug)]
+impl Matching {
+    fn new() -> Matching {
+        Matching {
+            matches: HashMap::new(),
+        }
+    }
+
+    fn insert(&mut self, x: String, y: String) -> Result<(), &str> {
+        if self.matches.contains_key(&x) || self.matches.contains_key(&y) {
+            return Err("Key already present in HashMap");
+        }
+
+        self.matches.insert(x.clone(), y.clone());
+        self.matches.insert(y, x);
+    
+        Ok(())
+    }
+
+    fn remove(&mut self, x: String, y: String) -> Result<(), &str> {
+        if self.matches.contains_key(&x) || self.matches.contains_key(&y) {
+            return Err("Key already present in HashMap");
+        }
+
+        self.matches.remove(&x);
+        self.matches.remove(&y);
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Clone)]
 struct Rankings {
-    x_ranking: HashMap<&'static str, Vec<&'static str>>,
-    y_ranking: HashMap<&'static str, Vec<&'static str>>,
+    x_ranking: HashMap<String, Vec<String>>,
+    y_ranking: HashMap<String, Vec<String>>,
 }
 
 impl Rankings {
@@ -17,67 +48,88 @@ impl Rankings {
         todo!();
     }
 
-    // solve simple SMP using gale-shapley
-    fn solve_smp(&self) -> Matching {
+    fn remove_next_y(&mut self, x: &String) -> Option<String> {
+        // for a given "x", fn returns the next preferred "y"
+        // also removes y from x's ranking, to avoid repeating challenges to the same y 
+        let x_ranking = self.x_ranking
+            .get_mut(x)
+            .unwrap();
 
-        // clone a mutable copy of self
-        let mut rankings = self.clone();
-
-        // mutable list of available x
-        let mut x_available: Vec<&str> = self.x_ranking.keys().map(|s| *s).collect();
-
-        let mut matching = Matching {
-            matches: HashMap::new(),
-        };
-
-        // while x_available is not empty, continue
-        while !x_available.is_empty() {
-            let x = x_available.remove(0).unwrap();
-
-            // get x next best target
-            // deal with unwrap
-            // deal with remove being more costly than pop
-            let y = rankings.x_ranking.get(x).unwrap().remove(0);
-
-            // matches is key-value of y,x
-            // check if y is not already matched
-            if !matching.matches.contains_key(y) {
-                matching.matches.insert(y, x);
-            
-            // compete with current rival
-            } else {
-                let rival = *matching.matches.get(y).unwrap();
-
-            //     if women_preferences[woman].index(man) < women_preferences[woman].index(current_man):
-            //     engaged[woman] = man
-            //     men_free.append(current_man)
-            // else:
-            //     men_free.append(man)
-
-            }
-
-        Matching {
-            matches: HashMap::new(),
+        if x_ranking.is_empty() {
+            return None;
+        } else {
+            return Some(x_ranking.remove(0));
         }
-        
+
     }
 
-    // initialize Rankings with example data
-    fn example_init() -> Rankings {
-        let x_ranking = vec![
-            ("x1", vec!["y1", "y2", "y3"]),
-            ("x2", vec!["y2", "y1", "y3"]),
-            ("x3", vec!["y1", "y3", "y2"]),
-        ].into_iter().collect();
+    // fn create ranking_iter 
+    // consumable iter to use the next function which will help with remove_next_y...
+    // could be another struct altogether 
 
-        let y_ranking = vec![
-            ("y1", vec!["x2", "x1", "x3"]),
-            ("y2", vec!["x1", "x2", "x3"]),
-            ("y3", vec!["x3", "x1", "x2"]),
-        ].into_iter().collect();
+    fn check_incumbent_preferred(self, y: &String, challenger_x: &String, incumbent_x: &String) -> bool {
+        let ranking = self.y_ranking.get(y).unwrap();
 
-        Rankings { x_ranking: x_ranking, y_ranking: y_ranking }
-    }    
+
+        // get rank should be a separate function entirely
+         // Find the index of the element 30
+        // if let Some(index) = my_vec.iter().position(|&x| x == 30) {
+        //     println!("Index of 30: {}", index);
+        // } else {
+        //     println!("Element not found");
+        // }   
+    }
+
+    // solve simple SMP using gale-shapley
+    fn solve_smp(self) -> Matching {
+        let mut rankings = self.clone();
+
+        let mut matching = Matching::new();
+
+        let mut available_x: Vec<String> = self.x_ranking.keys().map(|s| s.to_owned()).collect();
+
+        while !available_x.is_empty() {
+            // multiple remove and pop, consider implementing available_x with DeQue instead of vec
+            let challenger_x = available_x.remove(0);
+
+            // todo: what happens if None?
+            let y = rankings.remove_next_y(&challenger_x).unwrap();
+
+            if matching.matches.contains_key(&y) {
+                let incumbent_x = matching.matches.get(&y).unwrap().to_owned();
+
+                
+                
+
+                //     if women_preferences[woman].index(man) < women_preferences[woman].index(current_man):
+                //     engaged[woman] = man
+                //     men_free.append(current_man)
+                // else:
+                //     men_free.append(man)
+
+            } else {
+                // y is currently not engaged
+                matching.insert(challenger_x, y);
+            }
+        }
+        matching
+    }
+
+    // fn example_init() -> Rankings {
+    //     let x_ranking = vec![
+    //         ("x1", vec!["y1", "y2", "y3"]),
+    //         ("x2", vec!["y2", "y1", "y3"]),
+    //         ("x3", vec!["y1", "y3", "y2"]),
+    //     ].into_iter().collect();
+
+    //     let y_ranking = vec![
+    //         ("y1", vec!["x2", "x1", "x3"]),
+    //         ("y2", vec!["x1", "x2", "x3"]),
+    //         ("y3", vec!["x3", "x1", "x2"]),
+    //     ].into_iter().collect();
+
+    //     Rankings { x_ranking, y_ranking }
+    // }
 
 }
 
